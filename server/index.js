a778251e66773a1b520d2cd60adb48d9ff1cd0ea
drunker121 <87312371+drunker121@users.noreply.github.com>
2023-01-08 01:46:30 +0530

    port added

--- server/index.js
+++ server/index.js
@@ -4,6 +4,8 @@ const app = express();
 const dotenv = require('dotenv');
 dotenv.config({path: './config.env'});
 
+const PORT = process.env.PORT;
+
 const mongoose = require('mongoose');
 const mongodb = require('./mongodb')
 mongodb();
